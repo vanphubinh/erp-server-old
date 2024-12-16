7184cbd931ab54cee6f9c2fc8f3e709fc66dd0bb
An <an.tran511@outlook.com>
2024-12-16 23:29:07 +0700

    fix: fix product template migraiont and table name

--- domain/src/product/product_template.rs
+++ domain/src/product/product_template.rs
@@ -5,7 +5,7 @@ use sea_orm::{entity::prelude::*, FromQueryResult, Set};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
-#[sea_orm(table_name = "uom")]
+#[sea_orm(table_name = "product_template")]
 #[serde(rename_all = "camelCase")]
 pub struct Model {
   #[sea_orm(primary_key, auto_increment = false)]
